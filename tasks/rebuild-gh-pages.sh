d62a88cd95f38b2add34537371f6ef4778799f29
rjz <rj@rjzaworski.com>
2014-08-25 21:50:45 -0700

    Fixes pages deps

--- tasks/rebuild-gh-pages.sh
+++ tasks/rebuild-gh-pages.sh
@@ -3,14 +3,17 @@
 set -e
 
 # Files that need to be checked out from master
-masterDeps='package.json tasks index.js'
+masterDeps='package.json tasks src'
 
 git checkout gh-pages
+
+rm -rf $masterDeps
+
 git checkout master $masterDeps
 
 # Rebuild documentation and clean deps
 npm run docs
-cp docs/* .
+cp -r docs/* .
 rm -rf $masterDeps
 
 # Commit and push
